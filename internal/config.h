5bf0afd3140b4a44ba1c5756c7806084f7b53fea
Masaaki Goshima <goccy54@gmail.com>
2020-02-22 12:01:55 +0900

    Add binding code

--- internal/config.h
+++ internal/config.h
@@ -116,10 +116,10 @@
 /* #undef HAVE_ERROR_T */
 
 /* Define if you have the expat library */
-//#define HAVE_EXPAT 1
+#define HAVE_EXPAT 1
 
 /* Define to 1 if you have the <expat.h> header file. */
-//#define HAVE_EXPAT_H 1
+#define HAVE_EXPAT_H 1
 
 /* Define to 1 if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
